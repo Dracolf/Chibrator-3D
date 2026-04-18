e900a9c2f7cd99e08a0eab2da303717a429328ff
Dracolf <florparab@gmail.com>
2026-04-19 00:56:36 +0200

    feat: add player model and basic mouvements for beginning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private PlayerInputController _playerInputController;
+
+    private void Awake()
+    {
+        _playerInputController = GetComponent<PlayerInputController>();
+    }
+
+    private void Update()
+    {
+        Vector3 positionChange = new Vector3(
+            _playerInputController.MovementInputVector.x,
+            0,
+            _playerInputController.MovementInputVector.y)
+            * Time.deltaTime
+            * _speed;
+
+        transform.position += positionChange;
+    }
+}
